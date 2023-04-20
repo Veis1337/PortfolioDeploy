c0dcf123c4cc87e59b1b50be78f4439579052735
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-04-20 13:02:50 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PortfolioHeroku
+# Michael Weiss React Portfolio Page
+
+## Description
+
+This project is a portfolio page for myself to showcase the projects that I have built to potential employers. 
+The motivation behind this project was to blow the HTML/CSS portfolio I built 5 months ago out of the water.
+In this project I was able to implement tailwind over vanilla CSS, use React components to structure the page, and 
+provide functionality I couldn't have dreamed up at the beginning of my coding journey.
+
+## Installation
+
+Deployed at http://Veis1337.github.io/ReactPortfolio
+
+## Usage
+
+![](./src/assets/Screenshot%202023-04-19%20132204.png)
+
+## Credits
+
+## License
+
+MIT © Michael Weiss
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/react.png";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#363b4e] text-[#f6e5e5]">
+      <div>
+        <img src={Logo} alt="React Logo" style={{ width: "50px" }} />
+      </div>
+
+      {/*Menu*/}
+      <div>
+        <ul className="hidden md:flex">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="projects" smooth={true} duration={500}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/*Hamburger, only shows on mobile and brings up larger nav menu*/}
+      <div onClick={handleClick} className="md:hidden z-10 hover:cursor-pointer">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/*Mobile Menu*/}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#363b4e] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/*Social Icons*/}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-[#f6e5e5]"
+              href="https://www.linkedin.com/in/the-michael-weiss/"
+              target="_blank"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-[#f6e5e5]"
+              href="https://github.com/Veis1337"
+              target="_blank"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-[#712ba0]">
+            <a
+              className="flex justify-between items-center w-full text-[#f6e5e5]"
+              href="mailto:weissm09@gmail.com"
+              target="_blank"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-[#5e6138]">
+            <a
+              className="flex justify-between items-center w-full text-[#f6e5e5]"
+              href="MichaelWeiss-2023.pdf"
+              target="_blank"
+              download
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
